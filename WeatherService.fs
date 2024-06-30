f7d581d9437e507965a51d01e42b339dc1b2a4f5
Houston Haynes (Rower) <8174976+houstonhaynes@users.noreply.github.com>
2024-06-30 15:32:56 -0500

    final geofence helper

--- WeatherService.fs
+++ WeatherService.fs
@@ -142,56 +142,25 @@ let getLatLon (coord: Coordinate) =
     let latitude = coord.Latitude.DecimalDegree * 1e6 |> round |> fun x -> x / 1e6
     let longitude = coord.Longitude.DecimalDegree * 1e6 |> round |> fun x -> x / 1e6
     (latitude, longitude)
-
-let createHalfCircle (latitude: float, longitude: float, radius: float) =
-    let originalCenter = Coordinate(latitude, longitude)
-    let center = Coordinate(latitude, longitude)
-    center.Move((radius * 1000.0), 0, Shape.Sphere)
-    let gd = GeoFence.Drawer(center, Shape.Sphere, 280)
-    let points = [getLatLon originalCenter] // add original center as the first point
-    for i in 0..5 do
-        let angle = (i - 30 + 360) % 360
-        gd.Draw(Distance(radius/2.0), angle)
-    gd.Close()
-    let points = points @ (gd.Points |> List.ofSeq |> List.map getLatLon) // add the rest of the points
-    let points = points |> List.take (List.length points - 1) // remove the last point
-    let points = points @ [getLatLon originalCenter] // add original center as the last point
-    points
-//    |> List.map (fun (lat, lon) -> $"{lat},{lon},")
-//    |> String.concat "\n"
-
-// This code is used to create an east-facing cone centered on the specified latitude and longitude    
-let createCone (latitude: float, longitude: float, radius: float) =
-    let originalCenter = Coordinate(latitude, longitude)
-    let center = Coordinate(latitude, longitude)
-    center.Move((radius * 1000.0), 290, Shape.Sphere) 
-    let gd = GeoFence.Drawer(center, Shape.Sphere, 210)
-    let points = [getLatLon originalCenter] 
-    for i in 0..4 do
-        let angle = (i - 10 + 360) % 360
-        gd.Draw(Distance(radius/8.0), angle)
-    gd.Close()
-    let points = points @ (gd.Points |> List.ofSeq |> List.map getLatLon) 
-    let points = points |> List.take (List.length points - 1) 
-    let points = points @ [getLatLon originalCenter] 
-    points
-//    |> List.map (fun (lat, lon) -> $"{lat},{lon},") // format the points as multi-line strings to verify at this site:
-//    |> String.concat "\n"                           // https://mobisoftinfotech.com/tools/plot-multiple-points-on-map/
-
-//let createTriangle (latitude: float, longitude: float, radius: float, heading: float) =
-    // plot center point as the first gd.Draw with a distance of 0 and angle of the heading
-    // gd.Draw a point the length of the radius along the direction of the heading + 15 degrees
-    // set the angle to heading - 60 and draw a point 1/4th of the radius length
-    // close the GeoFence.Drawer
     
-let createTriangle (latitude: float, longitude: float, radius: float, heading: float) =
-    let originalCenter = Coordinate(latitude, longitude)
-    let gd = GeoFence.Drawer(originalCenter, Shape.Sphere, 210)
-    // Draw a point the length of the radius along the direction of the heading + 15 degrees
-    gd.Draw(Distance(radius), (heading + 15.0) % 360.0)
-    gd.Draw(Distance(radius), (heading - 15.0) % 360.0)
-    gd.Close()
-    let points = gd.Points |> List.ofSeq |> List.map getLatLon // add the rest of the points
-    points
+let calculateNewPoint (point : GeoFence.Point, headingDeg: double, distanceKm: double) =
+    let newCoordinate = new Coordinate(point.Latitude, point.Longitude)
+    newCoordinate.Move(distanceKm * 1000.0, headingDeg, Shape.Sphere)
+    let newPoint = getLatLon newCoordinate
+    GeoFence.Point(newPoint |> fst, newPoint |> snd)
     
-    //35.58166423535203, -82.5574313954071
+let createPieSlice (latA: float, lonA: float, radiusKm: float, bearingDeg: float, angleDeg: float) =
+    let startPoint = GeoFence.Point(latA, lonA)
+    let mutable points : List<GeoFence.Point> = []
+    points <- startPoint :: points // Prepend startPoint to the list
+    let bearingMin = (bearingDeg - (angleDeg / 2.0)) % 360.0
+    // Calculate and add intermediate points
+    for newBearing in [bearingMin..5.0..(bearingMin + angleDeg)] do
+        let newPoint = calculateNewPoint(startPoint, newBearing, radiusKm)
+        points <- newPoint :: points
+    points <- startPoint :: points // Postpend startPoint to the list
+    points
+    |> List.map (fun point -> $"{point.Latitude},{point.Longitude},") 
+    |> String.concat "\n"
+    
+// createPieSlice(35.581664, -82.557433, 5.0, 300, 40.0)
